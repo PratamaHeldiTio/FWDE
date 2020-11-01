86953d760d9c37f6ed40ed8752a5e38318d38fb2
pratamahelditio <pratamahelditio@gmail.com>
2020-11-01 17:50:31 +0700

    selesai menerapkan pwa

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = {
@@ -28,6 +29,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/templates/index.html'),
       filename: 'index.html',
+      favicon: 'src/public/images/icon.png',
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -39,6 +41,22 @@ module.exports = {
     }),
     new InjectManifest({
       swSrc: './src/scripts/sw.js',
+    }),
+    new WebpackPwaManifest({
+      name: 'EatGo Restaurant',
+      short_name: 'EatGo',
+      description: 'EatGo Restaurant tempat informasi restaurant modern nusantara',
+      start_url: '/index.html',
+      background_color: '#1e5f74',
+      display: 'standalone',
+      theme_color: '#1e5f74',
+      icons: [
+        {
+          src: './src/public/images/icon.png',
+          size: [72, 96, 128, 144, 192, 256, 284, 512],
+          purpose: 'any maskable',
+        },
+      ],
     }),
   ],
 };
